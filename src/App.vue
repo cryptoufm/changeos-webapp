eb69b4afe8c2d07365710d73f6b1813e145ac488
Edgar Godinez <edgarenrique.godinez@gmail.com>
2018-11-25 22:58:15 -0600

    VueRAVE

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,50 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app dark>
+      <v-toolbar-title class="headline text-uppercase">
+        <v-avatar
+          slot="activator"
+          size="40px"
+        >
+          <img
+            src="./assets/eos-logo.svg"
+            alt="Avatar"
+          >
+        </v-avatar>
+        <span> Changeos</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/cryptoufm"
+        target="_blank"
+      >
+        <span class="mr-2">Github Repo</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+    <v-footer class="pa-3" dark>
+      <v-spacer></v-spacer>
+    <div>&copy; EEGB | JRBN | JRAS | JLLG | MFGD {{ new Date().getFullYear() }}</div>
+  </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+import HelloWorld from "./components/HelloWorld";
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,130 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex xs12>
+
+        <!--
+        <v-img
+          :src="require('../assets/eos-logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+        -->
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Currently Casted Votes:
+        </h1>
+
+        
+        <v-data-table
+          :headers="headers"
+          :items="referendum"
+          class="elevation-1"
+        >
+
+          <template slot="items" slot-scope="props">
+            <tr @click="showIPFSImage(props.item)">
+              <td class="text-xs-left">{{ props.item.dpi }}</td>
+              <td class="text-xs-left">{{ props.item.vname }}</td>
+              <td class="text-xs-left">{{ props.item.ipfs }}</td>
+            </tr>
+          </template>
+        </v-data-table>
+
+        <v-btn color="info" v-on:click="getContractData()">Refresh</v-btn>
+
+        
+      </v-flex>
+
+    <v-dialog
+      v-model="dialog"
+      
+    >
+      <v-card>
+        <v-card-title class="headline">Stored Image in IPFS:</v-card-title>
+
+        <v-img
+          :src="QmYFXBeBEZ7QHMtuxJKdSa1iU6sViRqEoRYFoeSJegGU82"
+          aspect-ratio="1"
+          class="grey lighten-2"
+        >
+        </v-img>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            color="purple darken-1"
+            flat="flat"
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+      
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import axios from "axios";
+import Vue from 'vue';
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
+  data: () => ({
+    headers: [
+      {
+        text: "DPI #",
+        value: "dpi"
+      },
+      { text: "Volunteer Name", value: "vname" },
+      { text: "IPFS Hash", value: "ipfs" }
+    ],
+    referendum: [
+      {
+        dpi: "3001973810101",
+        vname: "eegod",
+        ipfs: "QmYFXBeBEZ7QHMtuxJKdSa1iU6sViRqEoRYFoeSJegGU82"
+      }
+    ],
+    dialog: false,
+    currentIPFSHash: "QmYFXBeBEZ7QHMtuxJKdSa1iU6sViRqEoRYFoeSJegGU82",
+  }),
+  //QmYFXBeBEZ7QHMtuxJKdSa1iU6sViRqEoRYFoeSJegGU82
+  methods: {
+    getContractData() {
+      axios
+        .get("http://35.231.64.75/referendum")
+        .then(response => {
+          let contractResponse = response.data.rows;
+          let array = []
+          for (let element in contractResponse){
+            console.log(contractResponse[element])
+            array.push(contractResponse[element])
+          }
+          Vue.set(this.$data, "referendum", array)
+          console.log(this.referendum)
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    showIPFSImage(row){
+      //Vue.set(this.$data, "currentIPFSHash", row.ipfs)
+      Vue.set(this.$data, "dialog", true);
+    }
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
 </style>
